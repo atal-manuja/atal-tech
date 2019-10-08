ba3cfc5168375353f359a88d9390914c4f297897
Atal Manuja <atalmanuja@MacBook-Air-2.local>
2019-10-08 20:20:03 +0530

    static added for deployment

--- atal_tech_apis/atal_tech_apis/settings.py
+++ atal_tech_apis/atal_tech_apis/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import django_heroku
+import dj_database_url
+
 django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
