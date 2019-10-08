d29c1a5b26b4c5a724054b9d4eb1951628a52cf1
Atal Manuja <atalmanuja@MacBook-Air-2.local>
2019-10-08 19:59:24 +0530

    static added for deployment

--- atal_tech_apis/atal_tech_apis/settings.py
+++ atal_tech_apis/atal_tech_apis/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'atal_tech_apis.urls'
