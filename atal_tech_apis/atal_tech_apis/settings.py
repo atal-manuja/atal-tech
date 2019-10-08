7b93fd0d2ac62b06b2c1fb3432f42522168597f3
Atal Manuja <atalmanuja@MacBook-Air-2.local>
2019-10-08 20:21:09 +0530

    static added for deployment

--- atal_tech_apis/atal_tech_apis/settings.py
+++ atal_tech_apis/atal_tech_apis/settings.py
@@ -14,7 +14,6 @@ import os
 import django_heroku
 import dj_database_url
 
-django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -138,4 +137,5 @@ STATICFILES_DIRS = (
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+django_heroku.settings(locals())
 
